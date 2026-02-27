9d0d5b73b2519510ceddcd90a4bc01602689ad53
renatodsilva <renato.ssantana2007@gmail.com>
2026-02-27 00:52:31 -0300

    aula 6 - coersão de tipos

---
+++ Exercicios/aula6.py
@@ -0,0 +1,8 @@
+#coerção/converção de tipos
+#converter tipos em outros
+#tipos imutáveis e primitivos:
+#str, int, float, bool
+print(int('1'),type(int('1')))
+print(float('1')+1)
+print(bool(' '))
+print(str(11)+'b')
