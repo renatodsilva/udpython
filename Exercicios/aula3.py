8ff9b5363cd0995bd1b8b8d756303e302c755594
renatodsilva <renato.ssantana2007@gmail.com>
2026-02-27 00:28:32 -0300

    aula 3 - Tipo str(string)

---
+++ Exercicios/aula3.py
@@ -0,0 +1,14 @@
+#Tipo str (String)
+#Strings são textos que estão dentro de aspas
+print(1234)
+
+#Aspas simples
+print('python')
+print(2,'"python"')
+
+#Aspas Duplas
+print("python")
+
+#Escape
+#(pula o próximo caractere)
+print('python\'teste\'')
