d010ef752946700556c89939392b1a8ad27b2da2
renatodsilva <renato.ssantana2007@gmail.com>
2026-02-27 18:22:37 -0300

    aula 8 - exercício variáveis

---
+++ Exercicios/aula8.py
@@ -0,0 +1,12 @@
+nome='Renato'
+sobrenome='da Silva Santana'
+idade=19
+ano_nascimento=2007
+maior_de_idade=idade>=18
+altura_metros=1.80
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de Nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
