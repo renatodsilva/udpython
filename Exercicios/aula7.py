a7667dda33adcc2b24a9a85cf6da885ec0654167
renatodsilva <renato.ssantana2007@gmail.com>
2026-02-27 18:12:12 -0300

    aula 7 - introdução a variáveis

---
+++ Exercicios/aula7.py
@@ -0,0 +1,11 @@
+#váriaveis são usadas para salvar algo na memória do computador
+#o sinal = é o operador de atribuição
+nome_completo='Renato da Silva Santana'
+soma_dois_mais_dois=2+2
+print(nome_completo,soma_dois_mais_dois)
+
+nome='Renato'
+idade=19
+maior_de_idade=idade>=18
+print('Nome:', nome)
+print('É maior?', maior_de_idade)
