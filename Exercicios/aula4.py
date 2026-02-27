d5073e6cea5f000ac0fc0fd74173882ee80588af
renatodsilva <renato.ssantana2007@gmail.com>
2026-02-27 00:36:46 -0300

    aula 4 - tipo int e float

---
+++ Exercicios/aula4.py
@@ -0,0 +1,13 @@
+#Tipo int e float
+#int são números inteiros
+print(1)
+print(-1)
+print(0)
+
+#float são números com ponto flutuante
+print(1.1, 10.11)
+
+#a função type mostra o tipo do valor
+print(type('python'))
+print(type(000))
+print(type(1.1),type(-1.1))
