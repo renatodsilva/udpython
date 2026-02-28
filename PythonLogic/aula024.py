df7aff61c410c48a85d9d991f0c7ec16bcd97dfe
renatodsilva <renato.ssantana2007@gmail.com>
2026-02-28 14:07:49 -0300

    aula 24 - operadores in e not in

---
+++ PythonLogic/aula024.py
@@ -0,0 +1,12 @@
+#Operadores in e not in
+# 0 1 2 3 4 5
+# r e n a t o
+#-6-5-4-3-2-1
+
+nome='renato'
+print(nome[2])
+print(nome[-4])
+
+print('a' in nome)
+print('z' in nome) 
+print('zero' not in nome)
