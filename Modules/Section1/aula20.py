cfd630e10af10243a803b8aac58007cbb4e97fdc
renatodsilva <renato.ssantana2007@gmail.com>
2026-02-28 02:55:07 -0300

    aula 20 - exercício de programação com if e comparação

---
+++ Modules/Section1/aula20.py
@@ -0,0 +1,7 @@
+primeiro_valor=input('Digite um valor: ')
+segundo_valor=input('Digite outro valor: ')
+
+if primeiro_valor>segundo_valor:
+    print(f'o primeiro valor:{primeiro_valor} é maior do que o segundo valor:{segundo_valor}')
+else: 
+    print(f'o segundo valor:{segundo_valor} é maior do que o primeiro valor:{primeiro_valor}')
