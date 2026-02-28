0c6fbfa3c7d35df62843565a56ea28a55eac1d10
renatodsilva <renato.ssantana2007@gmail.com>
2026-02-28 01:48:45 -0300

    aula 15 - função input

---
+++ Exercicios/aula015.py
@@ -0,0 +1,7 @@
+#função input
+nome=input('Digite o seu nome: ')
+print(f'O seu nome é {nome}')
+
+numero_1=int(input('Digite um número: '))
+numero_2=int(input('Digite outro número: '))
+print(f'A soma dos dois números é {numero_1+numero_2}')
