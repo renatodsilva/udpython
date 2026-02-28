9526a6c3063071b0ce64e0eb4a0b8cf158bf630f
renatodsilva <renato.ssantana2007@gmail.com>
2026-02-28 14:38:27 -0300

    aula 28 - exercício: teste de conhecimento

---
+++ PythonLogic/aula028.py
@@ -0,0 +1,31 @@
+"""
+Exercício
+Peça ao usuário para digitar seu nome
+Peça ao usuário para digitar sua idade
+Se nome e idade forem digitados:
+    Exiba:
+        Seu nome é {nome}
+        Seu nome invertido é {nome invertido}
+        Seu nome contém (ou não) espaços
+        Seu nome tem {n} letras
+        A primeira letra do seu nome é {letra}
+        A última letra do seu nome é {letra}
+Se nada for digitado em nome ou idade: 
+    exiba "Desculpe, você deixou campos vazios."
+"""
+nome=input('Digite seu nome: ')
+idade=input('Digite sua idade: ')
+
+if not nome or not idade:
+    print('Desculpe, você deixou campos vazios.')
+    exit()
+else:
+    print(f'Seu nome é {nome}')
+    print(f'Seu nome invertido é {nome[::-1]}')
+    if ' ' in nome:
+        print(f'Seu nome contem espaços')
+    else:
+        print(f'Seu nome não contem espaços')
+    print(f'Seu nome tem {len(nome)} letras')
+    print(f'A primeira letra do seu nome é {nome[:1]}')
+    print(f'A ultima letra do seu nome é {nome[-1]}')
